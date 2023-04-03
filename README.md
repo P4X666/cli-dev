1116d58f55aacb440db84d2756644302c67fe033
pengshixing <2644378911@qq.com>
2023-04-03 21:59:24 +0800

    替换原有模块，安装新模块划分

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # cli-dev
 脚手架搭建
+## 核心流程 core
+
+## 命令 commands
+- 初始化
+- 发布
+- 清除缓存
+## 模型层 models
+- commands 命令
+- project 项目
+- component 组件
+- npm 模块
+- git 仓库
+## 支撑模块 utils
+- git 操作
+- 云构建
+- 工具方法
+- api请求
+- git api
---
+++ core/core/bin/index.js
@@ -0,0 +1,12 @@
+#! /usr/bin/env node
+
+// const yargs = require("yargs/yargs");
+// const { hideBin } = require('yargs/helpers');
+
+// const arg = hideBin(process.argv);
+
+// yargs(arg).argv;
+
+const utils = require("@cli-dev_p/utils");
+
+console.log(utils());
