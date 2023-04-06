6ede26226834577b815117b264012b4e0daba006
pengshixing <2644378911@qq.com>
2023-04-06 21:54:00 +0800

    检查node版本，若版本过低，则提示版本不兼容

---
+++ core/core/lib/constant.js
@@ -0,0 +1,6 @@
+/** 支持 node 最低版本号 */
+const LOWEST_NODE_VERSION = '16.0.0'
+
+module.exports = {
+  LOWEST_NODE_VERSION
+}
--- core/core/lib/index.js
+++ core/core/lib/index.js
@@ -1,12 +1,32 @@
 'use strict';
 
 module.exports = core;
-
-const pkg = require("../package.json");
+/** 比对版本号的大小 */
+const semver = require("semver");
+const colors = require("colors");
 const log = require("@cli-dev/log");
 
+const pkg = require("../package.json");
+const constant = require('./constant');
+
 function core(params) {
-  checkPkgVersion();
+  // 捕获堆栈信息，只打印出message
+  try {
+    checkPkgVersion();
+    checkNodeVersion();
+  } catch (error) {
+    log().error(error.message)
+  }
+  
+}
+function checkNodeVersion() {
+  // 获取当前Node版本号
+  console.log(process.version);
+  const currentNodeVersion = process.version;
+  // 比对最低版本号
+  if (!semver.gte(currentNodeVersion, constant.LOWEST_NODE_VERSION)) {
+    throw new Error(colors.red(`cli-dev 需要安装 ${constant.LOWEST_NODE_VERSION} 以上版本的nodejs`))
+  }
 }
 /** 检查版本号 */
 function checkPkgVersion() {
